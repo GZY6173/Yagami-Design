80e0c82c47d0ffc411d69d8d0e898c513af70900
guanziyu <you@example.com>
2018-12-07 19:59:08 +0800

    变一下

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Yagami-Design
-一个自用的组件库
+
+# react-native-yagami-design
+
+## Getting started
+
+`$ npm install react-native-yagami-design --save`
+
+### Mostly automatic installation
+
+`$ react-native link react-native-yagami-design`
+
+### Manual installation
+
+
+#### iOS
+
+1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
+2. Go to `node_modules` ➜ `react-native-yagami-design` and add `RNYagamiDesign.xcodeproj`
+3. In XCode, in the project navigator, select your project. Add `libRNYagamiDesign.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
+4. Run your project (`Cmd+R`)<
+
+#### Android
+
+1. Open up `android/app/src/main/java/[...]/MainActivity.java`
+  - Add `import com.reactlibrary.RNYagamiDesignPackage;` to the imports at the top of the file
+  - Add `new RNYagamiDesignPackage()` to the list returned by the `getPackages()` method
+2. Append the following lines to `android/settings.gradle`:
+  	```
+  	include ':react-native-yagami-design'
+  	project(':react-native-yagami-design').projectDir = new File(rootProject.projectDir, 	'../node_modules/react-native-yagami-design/android')
+  	```
+3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
+  	```
+      compile project(':react-native-yagami-design')
+  	```
+
+#### Windows
+[Read it! :D](https://github.com/ReactWindows/react-native)
+
+1. In Visual Studio add the `RNYagamiDesign.sln` in `node_modules/react-native-yagami-design/windows/RNYagamiDesign.sln` folder to their solution, reference from their app.
+2. Open up your `MainPage.cs` app
+  - Add `using Yagami.Design.RNYagamiDesign;` to the usings at the top of the file
+  - Add `new RNYagamiDesignPackage()` to the `List<IReactPackage>` returned by the `Packages` method
+
+
+## Usage
+```javascript
+import RNYagamiDesign from 'react-native-yagami-design';
+
+// TODO: What to do with the module?
+RNYagamiDesign;
+```
+  
--- Toast/loading.js
+++ Toast/loading.js
@@ -10,68 +10,68 @@ import {
     View
 } from 'react-native';
 
-import RootSiblings from 'react-native-root-siblings'
-import LottieView from 'lottie-react-native'
-let sibling
+// import RootSiblings from 'react-native-root-siblings'
+// import LottieView from 'lottie-react-native'
+// let sibling
 
-addLoading = (title) => {
-    sibling = new RootSiblings(
-        <View style={styles.containView}>
-            <View style={styles.loadingView}>
-                <LottieView
-                    source={require('./src/ic_loading.json')}
-                    style={{ width: 60, height: 60 }}
-                    autoPlay
-                    loop
-                    speed={1.5}
-                />
-            </View>
-            <Text style={styles.loadingText}>玩命加载中</Text>
-        </View>)
-    return sibling
-}
+// addLoading = (title) => {
+//     sibling = new RootSiblings(
+//         <View style={styles.containView}>
+//             <View style={styles.loadingView}>
+//                 <LottieView
+//                     source={require('./src/ic_loadingfind.json')}
+//                     style={{ width: 80, height: 80 }}
+//                     autoPlay
+//                     loop
+//                     speed={1.5}
+//                 />
+//             </View>
+//             <Text style={styles.loadingText}>玩命加载中</Text>
+//         </View>)
+//     return sibling
+// }
 
-export const Loading = () => {
-    addLoading()
-}
+// export const Loading = () => {
+//     addLoading()
+// }
 
-export const StopLoading = () =>{
-    sibling.destroy()
-}
+// export const StopLoading = () =>{
+//     sibling.destroy()
+// }
 
-const styles = StyleSheet.create({
-    containView: {
-        position: 'absolute',
-        top: 0,
-        height: Dimensions.get('window').height,
-        width: Dimensions.get('window').width,
-        alignItems: 'center',
-        justifyContent: 'center',
-        opacity: 0.7,
-        backgroundColor: '#000000',
-    },
-    toastView: {
-        paddingVertical: 12,
-        paddingHorizontal: 10,
-        backgroundColor: '#000000',
-        opacity: 0.7,
-        borderRadius: 8
-    },
-    toastText: {
-        fontSize: 17,
-        color: 'white'
-    },
-    loadingView: {
-        width: 100,
-        height: 100,
-        borderRadius: 6,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'white'
-    },
-    loadingText: {
-        color: 'white',
-        fontSize: 18,
-        marginTop: 15
-    }
-});
+// const styles = StyleSheet.create({
+//     containView: {
+//         position: 'absolute',
+//         top: 0,
+//         height: Dimensions.get('window').height,
+//         width: Dimensions.get('window').width,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         // opacity: 0.7,
+//         backgroundColor: '#00000088',
+//     },
+//     toastView: {
+//         paddingVertical: 12,
+//         paddingHorizontal: 10,
+//         backgroundColor: '#000000',
+//         opacity: 0.7,
+//         borderRadius: 8
+//     },
+//     toastText: {
+//         fontSize: 17,
+//         color: 'white'
+//     },
+//     loadingView: {
+//         width: 100,
+//         height: 100,
+//         borderRadius: 6,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         backgroundColor: 'white'
+//     },
+//     loadingText: {
+//         color: 'white',
+//         fontSize: 17,
+//         marginTop: 15
+//     }
+// });
