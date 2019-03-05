ec58022826dbad268d55942548918040ba5e8c2a
guanziyu <guanziyu@example.com>
2019-03-05 15:49:53 +0800

    修改一个bug

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * 基础组件
  */
-export { default as Text } from './Basic/Text/text'
-export { default as Button } from './Basic/Button/button'
+export { default as Text } from './Basic/Text'
+export { default as Button } from './Basic/Button'
 export { default as Toast } from './Basic/Toast'
 export { default as Icon } from './Basic/Icon'
 export { default as Modal } from './Basic/Modal'
