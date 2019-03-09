47653b28dda9d20ed2065408fea28fcaab020e27
guanziyu <guanziyu@example.com>
2019-03-09 11:55:41 +0800

    add api

---
+++ Api/Platform.js
@@ -0,0 +1,57 @@
+/**
+ * Created by zhongzihuan on 2017/7/27.
+ * 平台兼容处理工具类
+ */
+import { Platform, Dimensions } from 'react-native'
+
+// 屏幕大小比例
+const DEVICE_WIDTH_DP = Dimensions.get('window').width
+const DEVICE_HEIGHT_DP = Dimensions.get('window').height
+
+export default class PlatformUtils {
+
+	/**
+	 * 判断是否为iphonex
+	 * @returns {boolean}
+	 */
+    static isIphoneX = () => {
+        return PlatformUtils.isIOS() && DEVICE_WIDTH_DP === 375 && DEVICE_HEIGHT_DP === 812
+    }
+
+	/**
+	 * web
+	 * @returns {boolean}
+	 */
+    static isWeb = () => {
+        if (Platform.OS == 'web') {
+            return true
+        } else {
+            return false
+        }
+    }
+
+	/**
+	 * 判断当前系统是否为android
+	 * @returns {boolean}
+	 */
+    static isAndroid = () => {
+        if (Platform.OS == 'android') {
+            return true
+        } else {
+            return false
+        }
+    }
+
+	/**
+	 * 判断当前系统是否为IOS
+	 * @returns {boolean}
+	 */
+    static isIOS = () => {
+        if (Platform.OS == 'ios') {
+            return true
+        } else {
+            return false
+        }
+    }
+
+}
--- index.js
+++ index.js
@@ -5,3 +5,10 @@ export { default as Text } from './Basic/Text'
 export { default as Toast } from './Basic/Toast'
 export { default as Icon } from './Basic/Icon'
 
+
+
+/**
+ * api
+ */
+export { default as Dimens } from './Api/dimens'
+export { default as Platform } from './Api/Platform'
