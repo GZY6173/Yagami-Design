c85ca45e29845038bb71e477aee329142bb99957
关子俞 <yagami_zero@163.com>
2020-05-27 15:16:18 +0800

    有个东西写错了

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /**
  * 基础组件
  */
-export { default as Text } from './Component/Text'
-export {default as Image} from './Component/Image'
-export { default as Toast } from './Component/Toast'
-export { default as Icon } from './Component/Icon'
-export { default as View } from './Component/View'
-export { default as Modal } from './Component/Modal'
+export { default as Text } from './component/Text'
+export { default as Image } from './component/Image'
+export { default as Toast } from './component/Toast'
+export { default as Icon } from './component/Icon'
+export { default as View } from './component/View'
+export { default as Modal } from './component/Modal'
 
-export { default as FlatList } from './Component/FlatList'
-export { default as ActivityIndicator } from './Component/ActivityIndicator'
-export { default as ImageBackground } from './Component/ImageBackground'
-export { default as KeyboardAvoidingView } from './Component/KeyboardAvoidingView'
-export { default as MaskedView } from './Component/MaskedView'
-export { default as Picker } from './Component/Picker'
-export { default as RefreshControl } from './Component/RefreshControl'
-export { default as ScrollView } from './Component/ScrollView'
-export { default as SectionList } from './Component/SectionList'
-export { default as Switch } from './Component/Switch'
-export { default as TextInput } from './Component/TextInput'
-export { default as WebView } from './Component/WebView'
+export { default as FlatList } from './component/FlatList'
+export { default as ActivityIndicator } from './component/ActivityIndicator'
+export { default as ImageBackground } from './component/ImageBackground'
+export { default as KeyboardAvoidingView } from './component/KeyboardAvoidingView'
+export { default as MaskedView } from './component/MaskedView'
+export { default as Picker } from './component/Picker'
+export { default as RefreshControl } from './component/RefreshControl'
+export { default as ScrollView } from './component/ScrollView'
+export { default as SectionList } from './component/SectionList'
+export { default as Switch } from './component/Switch'
+export { default as TextInput } from './component/TextInput'
+export { default as WebView } from './component/WebView'
 
 /**
  * api
